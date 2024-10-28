9e5a74becb9e7f5d34568e3f53697945d97efa8c
Tany Paushkina <moybotinok@gmail.com>
2024-10-28 12:39:14 +0300

    Init commit

---
+++ LivePictures/LivePicturesTests/LivePicturesTests.swift
@@ -0,0 +1,17 @@
+//
+//  LivePicturesTests.swift
+//  LivePicturesTests
+//
+//  Created by Tany on 28.10.2024.
+//
+
+import Testing
+@testable import LivePictures
+
+struct LivePicturesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
